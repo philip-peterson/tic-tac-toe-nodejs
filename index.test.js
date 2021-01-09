d8aa81f8ae632814400d696e584b770092c9d615
Philip Peterson <philip.c.peterson@gmail.com>
2021-01-09 16:09:35 -0500

    Add more tests

--- index.test.js
+++ index.test.js
@@ -1,57 +1,59 @@
 const { Game, Ui } = require("./index");
 
-test("Game lifecycle should be reasonable", () => {
-  const game = new Game();
-  expect(game.getWinner()).toBe(null);
-  expect(game.turn).toBe("X");
-  game.makeMove({ row: 0, column: 0, player: "X" });
-  expect(() => {
-    // Should not be able to go in a taken space.
-    game.makeMove({ row: 0, column: 0, player: "O" });
-  }).toThrow();
-  expect(game.getWinner()).toBe(null);
-  expect(game.turn).toBe("O");
-  game.makeMove({ row: 1, column: 0, player: "O" });
-  expect(game.getWinner()).toBe(null);
-  game.makeMove({ row: 2, column: 2, player: "X" });
-  expect(game.getWinner()).toBe(null);
-  game.makeMove({ row: 0, column: 2, player: "O" });
-  expect(game.getWinner()).toBe(null);
-  game.makeMove({ row: 1, column: 1, player: "X" });
-  expect(game.getWinner()).toBe("X");
-});
+describe("Game lifecycle", () => {
+  it("Should handle happy path", () => {
+    const game = new Game();
+    expect(game.getWinner()).toBe(null);
+    expect(game.turn).toBe("X");
+    game.makeMove({ row: 0, column: 0, player: "X" });
+    expect(() => {
+      // Should not be able to go in a taken space.
+      game.makeMove({ row: 0, column: 0, player: "O" });
+    }).toThrow();
+    expect(game.getWinner()).toBe(null);
+    expect(game.turn).toBe("O");
+    game.makeMove({ row: 1, column: 0, player: "O" });
+    expect(game.getWinner()).toBe(null);
+    game.makeMove({ row: 2, column: 2, player: "X" });
+    expect(game.getWinner()).toBe(null);
+    game.makeMove({ row: 0, column: 2, player: "O" });
+    expect(game.getWinner()).toBe(null);
+    game.makeMove({ row: 1, column: 1, player: "X" });
+    expect(game.getWinner()).toBe("X");
+  });
 
-test("Should handle cat's game", () => {
-  const game = new Game();
-  expect(game.anyMovesLeft()).toBe(true);
-  game.makeMove({ row: 0, column: 0, player: "X" });
-  expect(game.anyMovesLeft()).toBe(true);
-  expect(game.getWinner()).toBe(null);
-  game.makeMove({ row: 0, column: 1, player: "O" });
-  expect(game.anyMovesLeft()).toBe(true);
-  expect(game.getWinner()).toBe(null);
-  game.makeMove({ row: 1, column: 1, player: "X" });
-  expect(game.anyMovesLeft()).toBe(true);
-  expect(game.getWinner()).toBe(null);
-  game.makeMove({ row: 2, column: 2, player: "O" });
-  expect(game.anyMovesLeft()).toBe(true);
-  expect(game.getWinner()).toBe(null);
-  game.makeMove({ row: 0, column: 2, player: "X" });
-  expect(game.anyMovesLeft()).toBe(true);
-  expect(game.getWinner()).toBe(null);
-  game.makeMove({ row: 1, column: 0, player: "O" });
-  expect(game.anyMovesLeft()).toBe(true);
-  expect(game.getWinner()).toBe(null);
-  game.makeMove({ row: 2, column: 1, player: "X" });
-  expect(game.anyMovesLeft()).toBe(true);
-  expect(game.getWinner()).toBe(null);
-  game.makeMove({ row: 2, column: 0, player: "O" });
-  expect(game.anyMovesLeft()).toBe(true);
-  expect(game.getWinner()).toBe(null);
-  expect(game.anyMovesLeft()).toBe(true);
-  game.makeMove({ row: 1, column: 2, player: "X" });
-  expect(game.getWinner()).toBe(null);
-  expect(game.anyMovesLeft()).toBe(false);
+  it("Should handle cat's game", () => {
+    const game = new Game();
+    expect(game.anyMovesLeft()).toBe(true);
+    game.makeMove({ row: 0, column: 0, player: "X" });
+    expect(game.anyMovesLeft()).toBe(true);
+    expect(game.getWinner()).toBe(null);
+    game.makeMove({ row: 0, column: 1, player: "O" });
+    expect(game.anyMovesLeft()).toBe(true);
+    expect(game.getWinner()).toBe(null);
+    game.makeMove({ row: 1, column: 1, player: "X" });
+    expect(game.anyMovesLeft()).toBe(true);
+    expect(game.getWinner()).toBe(null);
+    game.makeMove({ row: 2, column: 2, player: "O" });
+    expect(game.anyMovesLeft()).toBe(true);
+    expect(game.getWinner()).toBe(null);
+    game.makeMove({ row: 0, column: 2, player: "X" });
+    expect(game.anyMovesLeft()).toBe(true);
+    expect(game.getWinner()).toBe(null);
+    game.makeMove({ row: 1, column: 0, player: "O" });
+    expect(game.anyMovesLeft()).toBe(true);
+    expect(game.getWinner()).toBe(null);
+    game.makeMove({ row: 2, column: 1, player: "X" });
+    expect(game.anyMovesLeft()).toBe(true);
+    expect(game.getWinner()).toBe(null);
+    game.makeMove({ row: 2, column: 0, player: "O" });
+    expect(game.anyMovesLeft()).toBe(true);
+    expect(game.getWinner()).toBe(null);
+    expect(game.anyMovesLeft()).toBe(true);
+    game.makeMove({ row: 1, column: 2, player: "X" });
+    expect(game.getWinner()).toBe(null);
+    expect(game.anyMovesLeft()).toBe(false);
+  });
 });
 
 describe("Parse user input", () => {
@@ -68,10 +70,28 @@ describe("Parse user input", () => {
       player: "X",
     });
   });
+
   it("Should report out of bounds", () => {
     const game = new Game();
     expect(() => {
       Ui.parseMove(game, "-1 1");
+    }).toThrow();
+    expect(() => {
+      Ui.parseMove(game, "3 1");
+    }).toThrow();
+  });
+
+  it("Should report nonnumeric", () => {
+    const game = new Game();
+    expect(() => {
+      Ui.parseMove(game, "e 1");
+    }).toThrow();
+  });
+
+  it("Should report wrong number of args", () => {
+    const game = new Game();
+    expect(() => {
+      Ui.parseMove(game, "1");
     }).toThrow();
   });
 });
