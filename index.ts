a384e046a938df8fbaa9c699bf6adb90e5c5744a
Philip Peterson <philip.c.peterson@gmail.com>
2021-01-09 14:27:43 -0500

    Separation of concerns between Game and UI. Also, structure for Move.

--- index.ts
+++ index.ts
@@ -1,5 +1,18 @@
+const readline = require('readline');
+const process = require('process');
+
 type Player = 'X' | 'O';
 
+/// Represents a single move made by a particular player.
+class Move {
+    player: Player;
+    row: 0 | 1 | 2;
+    column: 0 | 1 | 2;
+}
+
+/// Encapsulates the state of the game, allowing
+/// input and stepping to be separate from the business
+/// logic and rules of play.
 class Game {
     // Player X goes first
     // Represents the current player
@@ -10,22 +23,36 @@ class Game {
         [null,null,null],
         [null,null,null],
     ];
-    
-    promptInput() {
+
+    isValidMove(Move move) {
 
     }
 
-    isValidMove() {
+    makeMove() {
+        if (!this.isValidMove(move)) {
+            throw new Error("Move not allowed");
+        }
+    }
+}
+
+/// Class to encapsulate the logic of accepting, parsing,
+/// and validating user input
+class Ui {
+    static promptInput(Game game) {
 
     }
 }
 
 const main = () => {
     const game = new Game();
+    const ioInterface = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
     while (true) {
         let move;
         try {
-            move = game.promptInput();
+            move = Ui.promptInput(game);
         } catch (e) {
             console.error(
                `Invalid instruction. Must be two numbers from 0-2 separated by a space.`
