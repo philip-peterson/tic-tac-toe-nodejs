1b750bd4485972d5a69439c9d2996e77a3ee297c
Philip Peterson <philip.c.peterson@gmail.com>
2021-01-09 16:06:33 -0500

    Tests for cat's game

--- index.test.js
+++ index.test.js
@@ -21,6 +21,39 @@ test("Game lifecycle should be reasonable", () => {
   expect(game.getWinner()).toBe("X");
 });
 
+test("Should handle cat's game", () => {
+  const game = new Game();
+  expect(game.anyMovesLeft()).toBe(true);
+  game.makeMove({ row: 0, column: 0, player: "X" });
+  expect(game.anyMovesLeft()).toBe(true);
+  expect(game.getWinner()).toBe(null);
+  game.makeMove({ row: 0, column: 1, player: "O" });
+  expect(game.anyMovesLeft()).toBe(true);
+  expect(game.getWinner()).toBe(null);
+  game.makeMove({ row: 1, column: 1, player: "X" });
+  expect(game.anyMovesLeft()).toBe(true);
+  expect(game.getWinner()).toBe(null);
+  game.makeMove({ row: 2, column: 2, player: "O" });
+  expect(game.anyMovesLeft()).toBe(true);
+  expect(game.getWinner()).toBe(null);
+  game.makeMove({ row: 0, column: 2, player: "X" });
+  expect(game.anyMovesLeft()).toBe(true);
+  expect(game.getWinner()).toBe(null);
+  game.makeMove({ row: 1, column: 0, player: "O" });
+  expect(game.anyMovesLeft()).toBe(true);
+  expect(game.getWinner()).toBe(null);
+  game.makeMove({ row: 2, column: 1, player: "X" });
+  expect(game.anyMovesLeft()).toBe(true);
+  expect(game.getWinner()).toBe(null);
+  game.makeMove({ row: 2, column: 0, player: "O" });
+  expect(game.anyMovesLeft()).toBe(true);
+  expect(game.getWinner()).toBe(null);
+  expect(game.anyMovesLeft()).toBe(true);
+  game.makeMove({ row: 1, column: 2, player: "X" });
+  expect(game.getWinner()).toBe(null);
+  expect(game.anyMovesLeft()).toBe(false);
+});
+
 describe("Parse user input", () => {
   it("Should handle positive input", () => {
     const game = new Game();
